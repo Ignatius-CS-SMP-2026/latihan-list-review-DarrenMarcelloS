cadf848812648c27521b6581a4cf705a92ede21e
DarrenMarcelloS <153891427+DarrenMarcelloS@users.noreply.github.com>
2026-03-06 15:16:21 +0700

    Update latihan_list.py

--- latihan_list.py
+++ latihan_list.py
@@ -1,5 +1,5 @@
-# NAMA  : 
-# KELAS : 
+# NAMA  : Darren Marcello Susanto
+# KELAS : 9A
 # ---------------------------------------------------------
 # LATIHAN: REVIEW LIST PYTHON
 #Diberikan sebuah data acak nilai ujian siswa. Buatlah program yang mengurutkan data tersebut 
@@ -16,3 +16,28 @@
 # ---------------------------------------------------------
 
 # Tulis kodemu di bawah ini:
+nilai_ujian = [75, 55, 90, 85, 45, 95, 80] 
+print("Data nilai asli: ", nilai_ujian)
+
+n = len(nilai_ujian)
+
+for i in range(n):
+    for j in range(0, n - i - 1):
+        if nilai_ujian[j] < nilai_ujian[j + 1]:   
+            nilai_ujian[j], nilai_ujian[j + 1] = nilai_ujian[j + 1], nilai_ujian[j]
+
+print("Hasil urut descending:", nilai_ujian)
+
+penerima_beasiswa = []
+
+for i in range (3):
+    penerima_beasiswa.append(nilai_ujian[i])
+
+print("Tiga nilai tertinggi (Penerima Beasiswa):", penerima_beasiswa)
+
+nilai_lulus = []
+for nilai in nilai_ujian :
+    if nilai >= 60:
+        nilai_lulus.append(nilai)
+
+print("Daftar nilai yang lulus: ", nilai_lulus)
